f0b92ecbbb94ecceed2ccf66d799767b9a700a91
Markus <ghsthack@gmail.com>
2018-09-08 15:29:34 +0200

    Looks like we have a table

--- fostatus-configed.rkt
+++ fostatus-configed.rkt
@@ -13,42 +13,84 @@
 ; Get server names, convert to list of strings and sort in alphanumerical order
 (define names (sort (map ~a (hash-keys all_servers)) string<?))
 
-; Print servers and defined keys
-;(for ([server names])
-   ;(define xx (map ~a (hash-keys (hash-ref all_servers (string->symbol server)))) )
-   ;(printf "~a: ~a\n" server (string-join xx ","))
-; )
+; Get a server property, or "" if it doesn't exist
+(define (server_get server)
+  (hash-ref all_servers (string->symbol server))
+  )
 
 
+(define (server_key server key)
+  (hash-ref (server_get server) key ""))
+(define (server_key_exists server key)
+  (hash-has-key? (server_get server) key))
+; Helpers to get all properties from server x
+(define (server_name server)(server_key server 'name))
+(define (server_host server)(server_key server 'host))
+(define (server_port server)(server_key server 'port))
+(define (server_web server) (server_key server 'website))
+(define (server_link server)(server_key server 'link))
+(define (server_irc server)(server_key server 'irc))
+(define (server_color server)(server_key server 'color))
+
+; host:port or ""
+(define (server_addr server)
+  (if (server_key_exists server 'host)
+      (format "~a:~a" (server_host server) (server_port server))
+      ""
+  )
+ )
+
+(define (html_href url text) `(a (( href ,url)) ,text))
+
+; Link to server
+;(define (server_href x) `(a (( href ,(format "/server/~a" x))) ,(server_key x 'name)) )
+(define (server_href x) (html_href (format "/server/~a" x) (server_name x)))
+  
 (define (include_css name)
   `(link ((rel "stylesheet") (href "style.css")))
   )
 
 (define (html_table_header elems)
-  `(,@(for/list ([x elems])
+  `(tr ,@(for/list ([x elems])
     `(th ,x)))
   )
 
-(html_table_header '("Test" "Hoho"))
+(define (html_table_row cells)
+`(tr ,@(for/list ([x cells])
+    `(td ,x ))))
 
 (define (html_table header rows)
-  `(table (thead ,@header) (tbody ,@rows))
+  `(table (thead (,@header) ) (tbody ,@rows))
 )
 
-(define server_links (map (lambda (x) `(a (( href ,(format "/server/~a" x)  ) (style "color: #8e9eae") ) ,x)) names))
-(define html_server_list (map (lambda (x) `(li ,x)) server_links))
+; Cells to show for each row.
+(define (server_attrs x) 
+  (list (server_href x)
+        (server_addr x)
+        (html_href (server_web x) (server_web x))
+  )
+)
 
-(include_css "test.css")
+;(define x (server_attrs "aftertimes"))
+;x
+
+(define server_rows (map (lambda (x) `,(html_table_row `,(server_attrs x))) names))
+server_rows
+
+(define server_table (html_table
+                              (html_table_header '("Name" "IP:Port", "Website"))
+                              server_rows
+  ))
+
+server_table
 
 (define (start req)
   (response/xexpr
    `(html (head (title "FOStatus Config-ed")
                 ,(include_css "style.css")) 
           (body (div ((id "content"))
-                     (div ((style "margin: auto;color: #8c8c8c; width: 500px; border: solid 3px #222; background: #1f1f1f; font-size: 26px; padding: 32px;" ))  (p "Click on a server to edit config.")
-                      (ul ,@html_server_list)
-                          ) )))
-                     ))
+                     (div ((id "edit-area" )) (p "Click on a server to edit config.")
+                      ,server_table))))))
 
 
 (serve/servlet start
@@ -56,19 +98,3 @@
                #:servlet-path "/"
                #:extra-files-paths (list(build-path "./static" ))
                )
-
-;(sort names string<?)
-;(define server_names list)
-;(for ( ((i) (in-hash-keys all_servers)) )
-;  (append (server_names i) )
-
-;(sort (hash-keys all_servers) string>?)
-;(define sorted (sort (all_servers string>?s) ))
-;sorted
-   ;(define server_names list)
-   ;(for ([server (hash-keys all_servers)])
-   ;    print server
-   ;    append (server_names server))
-
-;(for (((server_name props) (in-hash all_servers)))
-; (println server_name))
